64cb6b39ffcd706b636c596e58501c73625504a2
abhinavnagaraj <abhinav@spectrocloud.com>
2021-04-22 16:04:35 -0700

    update spectro manifests to include eks

--- spectro/run.sh
+++ spectro/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 rm ./generated/*
+rm ./global/generated/*
+
+
+# kustomize infra and managed separately to avoid multiple matches error
+kustomize build global/infra > ./global/generated/infra-global.yaml
+kustomize build global/managed > ./global/generated/managed-global.yaml
 
 kustomize build global > ./generated/core-global.yaml
 kustomize build base > ./generated/core-base.yaml
