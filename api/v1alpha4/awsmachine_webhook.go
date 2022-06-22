eedc908454c4669f448d622ce38a529ac7b82b14
Shubham Rajvanshi <shubham@spectrocloud.com>
2022-06-22 15:22:39 -0700

    Bet 4653 (#311)
    * BET-4653: root device name now defaults to ''
    
    Merging as discussed with Jun

--- api/v1alpha4/awsmachine_webhook.go
+++ api/v1alpha4/awsmachine_webhook.go
@@ -110,14 +110,6 @@ func (r *AWSMachine) ValidateUpdate(old runtime.Object) error {
 		delete(cloudInit, "secureSecretsBackend")
 	}
 
-	if rootVolume, ok := oldAWSMachineSpec["rootVolume"].(map[string]interface{}); ok {
-		delete(rootVolume,"deviceName")
-	}
-
-	if rootVolume, ok := newAWSMachineSpec["rootVolume"].(map[string]interface{}); ok {
-		delete(rootVolume,"deviceName")
-	}
-
 	if !reflect.DeepEqual(oldAWSMachineSpec, newAWSMachineSpec) {
 		s := fmt.Sprintf("oldAWSMachineSpec: %s, newAWSMachineSpec: %s do not match", oldAWSMachineSpec, newAWSMachineSpec)
 		allErrs = append(allErrs, field.Forbidden(field.NewPath("spec"), "cannot be modified."+ s))
@@ -210,6 +202,9 @@ func (r *AWSMachine) ValidateDelete() error {
 func (r *AWSMachine) Default() {
 	if !r.Spec.CloudInit.InsecureSkipSecretsManager && r.Spec.CloudInit.SecureSecretsBackend == "" {
 		r.Spec.CloudInit.SecureSecretsBackend = SecretBackendSecretsManager
+	}
+	if r.Spec.RootVolume != nil {
+		r.Spec.RootVolume.DeviceName = ""
 	}
 }
 
