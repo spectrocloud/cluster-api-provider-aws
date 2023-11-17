f359d5450b22a937692fdf7eb1e725dd9447fd4a
Will <30413278+wcrum@users.noreply.github.com>
2023-11-17 15:50:12 -0600

    Update podidentitywebhook.go

--- pkg/cloud/services/iam/podidentitywebhook.go
+++ pkg/cloud/services/iam/podidentitywebhook.go
@@ -9,6 +9,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
 	v12 "k8s.io/api/rbac/v1"
+	schedulingv1 "k8s.io/api/scheduling/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -163,7 +164,7 @@ func reconcilePodDistributionBudget(ctx context.Context, ns string, secret *core
 	pdb := &policyv1.PodDisruptionBudget{
 		ObjectMeta: objectMeta(podIdentityWebhookName, ns),
 		Spec: policyv1.PodDisruptionBudgetSpec{
-			MaxUnavailable: &intstr.IntOrString{
+			MinAvailable: &intstr.IntOrString{
 				Type:   intstr.Int,
 				IntVal: 1,
 			},
@@ -175,6 +176,32 @@ func reconcilePodDistributionBudget(ctx context.Context, ns string, secret *core
 		},
 	}
 	return remoteClient.Create(ctx, pdb)
+}
+
+func reconcilePriorityClass(ctx context.Context, ns string, secret *corev1.Secret, remoteClient client.Client) error {
+	check := &v13.Deployment{}
+	if err := remoteClient.Get(ctx, types.NamespacedName{
+		Name:      podIdentityWebhookName,
+		Namespace: ns,
+	}, check); err != nil && !apierrors.IsNotFound(err) {
+		return err
+	}
+
+	if check.UID != "" {
+		return nil
+	}
+
+	priorityClass := &schedulingv1.PriorityClass{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "capa-critical",
+			Namespace: ns,
+		},
+		Value:         890000000,
+		GlobalDefault: false,
+		Description:   "This priority class should be used for critical CAPA pods only.",
+	}
+
+	return remoteClient.Create(ctx, priorityClass)
 }
 
 func reconcileDeployment(ctx context.Context, ns string, secret *corev1.Secret, remoteClient client.Client) error {
@@ -207,7 +234,7 @@ func reconcileDeployment(ctx context.Context, ns string, secret *corev1.Secret, 
 					},
 				},
 				Spec: corev1.PodSpec{
-					PriorityClassName: "palette-spectro-cluster-critical",
+					PriorityClassName: "capa-critical",
 					Tolerations: []corev1.Toleration{
 						{
 							Effect:   corev1.TaintEffect("NoSchedule"),
@@ -343,6 +370,10 @@ func reconcilePodIdentityWebhookComponents(ctx context.Context, ns string, secre
 	}
 
 	if err := reconcilePodDistributionBudget(ctx, ns, secret, remoteClient); err != nil {
+		return err
+	}
+
+	if err := reconcilePriorityClass(ctx, ns, secret, remoteClient); err != nil {
 		return err
 	}
 
