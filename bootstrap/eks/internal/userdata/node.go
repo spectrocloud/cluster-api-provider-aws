8841540aec84fcb79d53e6073ec7c7e3d6f65781
Amit Sahastrabuddhe <amit.sahastra@gmail.com>
2025-08-01 21:41:24 +0530

    PCP-4952: EKS AL2023 Custom AMI with Dynamic Credentials(STS) Cluster Provisioning is failing

--- bootstrap/eks/internal/userdata/node.go
+++ bootstrap/eks/internal/userdata/node.go
@@ -25,6 +25,7 @@ import (
 	"github.com/alessio/shellescape"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/ptr"
+
 	eksbootstrapv1 "sigs.k8s.io/cluster-api-provider-aws/v2/bootstrap/eks/api/v1beta2"
 	"sigs.k8s.io/cluster-api-provider-aws/v2/exp/api/v1beta2"
 )
@@ -51,13 +52,7 @@ runcmd:
 {{- template "mounts" .Mounts}}
 `
 
-	// Common MIME header and boundary template
-	mimeHeaderTemplate = `MIME-Version: 1.0
-Content-Type: multipart/mixed; boundary="{{.Boundary}}"
-
-`
-
-	// Shell script part template for AL2023
+	// Shell script part template for AL2023.
 	shellScriptPartTemplate = `--{{.Boundary}}
 Content-Type: text/x-shellscript; charset="us-ascii"
 
@@ -75,7 +70,7 @@ set -o nounset
 {{- end}}
 {{- end}}`
 
-	// Node config part template for AL2023
+	// Node config part template for AL2023.
 	nodeConfigPartTemplate = `
 --{{.Boundary}}
 Content-Type: application/node.eks.aws
@@ -98,110 +93,7 @@ spec:
     - "--node-labels={{if and .KubeletExtraArgs (index .KubeletExtraArgs "node-labels")}}{{index .KubeletExtraArgs "node-labels"}}{{else}}eks.amazonaws.com/nodegroup-image={{if .AMIImageID}}{{.AMIImageID}}{{end}},eks.amazonaws.com/capacityType={{if .CapacityType}}{{.CapacityType}}{{else}}ON_DEMAND{{end}},eks.amazonaws.com/nodegroup={{.NodeGroupName}}{{end}}"
 
 --{{.Boundary}}--`
-
-	// AL2023-specific templates.
-	al2023KubeletExtraArgsTemplate = `{{- define "al2023KubeletExtraArgs" -}}
-{{- if . }}
-    - "--node-labels={{range $k, $v := .}}{{$k}}={{$v}}{{end}}"
-{{- end -}}
-{{- end -}}`
-
-	al2023ContainerRuntimeTemplate = `{{- define "al2023ContainerRuntime" -}}
-{{- if . -}}
-  containerRuntime: {{.}}
-{{- end -}}
-{{- end -}}`
-
-	al2023DockerConfigTemplate = `{{- define "al2023DockerConfig" -}}
-{{- if and . (ne . "''") -}}
-  dockerConfig: {{.}}
-{{- end -}}
-{{- end -}}`
-
-	al2023APIRetryAttemptsTemplate = `{{- define "al2023APIRetryAttempts" -}}
-{{- if . -}}
-  apiRetryAttempts: {{.}}
-{{- end -}}
-{{- end -}}`
-
-	al2023PauseContainerTemplate = `{{- define "al2023PauseContainer" -}}
-{{- if and .AccountNumber .Version -}}
-  pauseContainer:
-    accountNumber: {{.AccountNumber}}
-    version: {{.Version}}
-{{- end -}}
-{{- end -}}`
-
-	al2023FilesTemplate = `{{- define "al2023Files" -}}
-{{- if . -}}
-  files:{{ range . }}
-    - path: {{.Path}}
-      content: |
-{{.Content | Indent 8}}{{ if ne .Owner "" }}
-      owner: {{.Owner}}{{ end }}{{ if ne .Permissions "" }}
-      permissions: '{{.Permissions}}'{{ end }}{{ end }}
-{{- end -}}
-{{- end -}}`
-
-	al2023DiskSetupTemplate = `{{- define "al2023DiskSetup" -}}
-{{- if . -}}
-  diskSetup:{{ if .Partitions }}
-    partitions:{{ range .Partitions }}
-      - device: {{.Device}}
-        layout: {{.Layout}}{{ if .Overwrite }}
-        overwrite: {{.Overwrite}}{{ end }}{{ if .TableType }}
-        tableType: {{.TableType}}{{ end }}{{ end }}{{ end }}{{ if .Filesystems }}
-    filesystems:{{ range .Filesystems }}
-      - device: {{.Device}}
-        filesystem: {{.Filesystem}}
-        label: {{.Label}}{{ if .Partition }}
-        partition: {{.Partition}}{{ end }}{{ if .Overwrite }}
-        overwrite: {{.Overwrite}}{{ end }}{{ if .ExtraOpts }}
-        extraOpts:{{ range .ExtraOpts }}
-          - {{.}}{{ end }}{{ end }}{{ end }}{{ end }}
-{{- end -}}
-{{- end -}}`
-
-	al2023MountsTemplate = `{{- define "al2023Mounts" -}}
-{{- if . -}}
-  mounts:{{ range . }}
-    -{{ range . }}
-      - {{.}}{{ end }}{{ end }}
-{{- end -}}
-{{- end -}}`
-
-	al2023UsersTemplate = `{{- define "al2023Users" -}}
-{{- if . -}}
-  users:{{ range . }}
-    - name: {{.Name}}{{ if .Gecos }}
-      gecos: {{.Gecos}}{{ end }}{{ if .Groups }}
-      groups: {{.Groups}}{{ end }}{{ if .HomeDir }}
-      homeDir: {{.HomeDir}}{{ end }}{{ if .Inactive }}
-      inactive: {{.Inactive}}{{ end }}{{ if .Shell }}
-      shell: {{.Shell}}{{ end }}{{ if .Passwd }}
-      passwd: {{.Passwd}}{{ end }}{{ if .PrimaryGroup }}
-      primaryGroup: {{.PrimaryGroup}}{{ end }}{{ if .LockPassword }}
-      lockPassword: {{.LockPassword}}{{ end }}{{ if .Sudo }}
-      sudo: {{.Sudo}}{{ end }}{{ if .SSHAuthorizedKeys }}
-      sshAuthorizedKeys:{{ range .SSHAuthorizedKeys }}
-        - {{.}}{{ end }}{{ end }}{{ end }}
-{{- end -}}
-{{- end -}}`
-
-	al2023NTPTemplate = `{{- define "al2023NTP" -}}
-{{- if . -}}
-  ntp:{{ if .Enabled }}
-    enabled: true{{ end }}{{ if .Servers }}
-    servers:{{ range .Servers }}
-      - {{.}}{{ end }}{{ end }}
-{{- end -}}
-{{- end -}}`
 )
-
-// NodeUserData is responsible for generating userdata for EKS nodes.
-type NodeUserData struct {
-	input *NodeInput
-}
 
 // NodeInput contains all the information required to generate user data for a node.
 type NodeInput struct {
@@ -339,7 +231,7 @@ func generateAL2023UserData(input *NodeInput) ([]byte, error) {
 	var buf bytes.Buffer
 
 	// Write MIME header
-	if _, err := buf.WriteString(fmt.Sprintf("MIME-Version: 1.0\nContent-Type: multipart/mixed; boundary=\"%s\"\n\n", input.Boundary)); err != nil {
+	if _, err := buf.WriteString(fmt.Sprintf("MIME-Version: 1.0\nContent-Type: multipart/mixed; boundary=%q\n\n", input.Boundary)); err != nil {
 		return nil, fmt.Errorf("failed to write MIME header: %v", err)
 	}
 
@@ -363,7 +255,7 @@ func generateAL2023UserData(input *NodeInput) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// getCapacityTypeString returns the string representation of the capacity type
+// getCapacityTypeString returns the string representation of the capacity type.
 func (ni *NodeInput) getCapacityTypeString() string {
 	if ni.CapacityType == nil {
 		return "ON_DEMAND"
@@ -378,7 +270,7 @@ func (ni *NodeInput) getCapacityTypeString() string {
 	}
 }
 
-// validateAL2023Input validates the input for AL2023 user data generation
+// validateAL2023Input validates the input for AL2023 user data generation.
 func validateAL2023Input(input *NodeInput) error {
 	if input.APIServerEndpoint == "" {
 		return fmt.Errorf("API server endpoint is required for AL2023")
